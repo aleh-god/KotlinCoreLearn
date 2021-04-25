6d4e2262da39c82abb46f9f8d704009dbea935e4
aleh-god <aleh.god@gmail.com>
2021-04-25 11:58:08 +0300

    feat: add all parts CoreLearn
    CoreLearn Kotlin part complete

--- src/CoreLearn/AppCoreLearn.kt
+++ src/CoreLearn/AppCoreLearn.kt
@@ -1,13 +1,16 @@
-import CoreLearn.CoreLearn
-import CoreLearn.Operations
-import CoreLearn.Values
+import CoreLearn.*
 
 fun main(args: Array<String>){
     println("Hello Kotlin")
+
     var lesson: CoreLearn
 
     lesson = Values()
     lesson = Operations()
+    lesson = BooleanExp()
+    lesson = ConditionalConstructs()
+    lesson = CycleAndSequence()
+    lesson = ArrayLearn()
 
     lesson.showResult()
 
