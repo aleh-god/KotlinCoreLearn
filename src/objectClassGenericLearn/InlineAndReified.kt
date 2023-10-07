7be2516bb89acfb04c32b6dcf7bde8036b5981d7
aleh-god <aleh.god@gmail.com>
2023-10-07 15:45:35 +0300

    fix: code style

--- src/objectClassGenericLearn/InlineAndReified.kt
+++ src/objectClassGenericLearn/InlineAndReified.kt
@@ -7,27 +7,29 @@ fun main() {
     fun2("str")
     funStr2()
     funStr(fun3())
-
 }
-inline fun <reified T: Any> fun1(){
+
+inline fun <reified T: Any> fun1() {
     println("fun1 ${T::class.simpleName}")
 }
 
-inline fun <reified T: Any> fun2(t: T){
+inline fun <reified T: Any> fun2(t: T) {
     println("fun2 $t ${T::class.simpleName}")
 }
 
-inline fun <reified T: Any> fun3(): T{
+inline fun <reified T: Any> fun3(): T {
     if(T::class == String::class) println("fun3 ${T::class.simpleName}")
     exitProcess(0)
 }
 
-inline fun <reified T: Any> T.fun4(){
+inline fun <reified T: Any> T.fun4() {
     println("fun4 ${T::class.simpleName}")
 }
 
-fun funStr(str: String){}
+fun funStr(str: String) {
+    println("str = $str")
+}
 
-fun funStr2(){
+fun funStr2() {
     String.fun4()
 }
